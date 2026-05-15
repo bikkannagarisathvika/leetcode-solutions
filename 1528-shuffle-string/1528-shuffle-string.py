6ae68b32abe9fd7de4e40138292530abd266ea14
Bikkannagari Sathvika Reddy <sathvikareddy2226@gmail.com>
2026-05-15 21:58:43 +0530

    Time: 0 ms (100%), Space: 19.2 MB (91.13%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        res=[" "]*len(s)
+        for i in range(len(s)):
+            res[indices[i]]=s[i]
+        return''.join(res)
+            
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
