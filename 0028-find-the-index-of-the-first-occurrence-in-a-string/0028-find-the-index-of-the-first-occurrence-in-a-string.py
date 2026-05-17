b7e91920b249becef3f1d04927f2c75648b2b9f7
Bikkannagari Sathvika Reddy <sathvikareddy2226@gmail.com>
2026-05-17 20:30:54 +0530

    Time: 0 ms (100%), Space: 19.3 MB (26.79%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        try:
+	        return haystack.index(needle)
+        except:
+	        return -1
+
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
