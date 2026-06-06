d732085cd12461651535dc891dbd11dc5ab2fbab
Bikkannagari Sathvika Reddy <sathvikareddy2226@gmail.com>
2026-06-06 09:54:07 +0530

    Time: 0 ms (100%), Space: 19.4 MB (32.03%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        for i in range(len(strs[0])):
+            for s in strs[1:]:
+                if i==len(s) or s[i]!=strs[0][i]:
+                    return s[:i]
+        return strs[0]
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
