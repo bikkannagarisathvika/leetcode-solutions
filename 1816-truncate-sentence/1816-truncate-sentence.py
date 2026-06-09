318cb2526bffc5563d8cbe5e4527dfbc78d71d80
Bikkannagari Sathvika Reddy <sathvikareddy2226@gmail.com>
2026-06-09 13:53:07 +0530

    Time: 0 ms (100%), Space: 19.3 MB (58.88%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.py
@@ -0,0 +1,7 @@
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        return " ".join(s.split()[:k])
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
