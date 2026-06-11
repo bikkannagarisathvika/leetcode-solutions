fd9902966350e50167381d3b659c244cbedfbc18
Bikkannagari Sathvika Reddy <sathvikareddy2226@gmail.com>
2026-06-11 21:57:17 +0530

    Time: 0 ms (100%), Space: 19.3 MB (22.16%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
+        return nums
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
