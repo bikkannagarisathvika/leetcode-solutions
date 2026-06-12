91aa1bdf09bcff53fed848e9a1a176140d08cc45
Bikkannagari Sathvika Reddy <sathvikareddy2226@gmail.com>
2026-06-12 09:17:28 +0530

    Time: 3 ms (10.57%), Space: 19.3 MB (61.45%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,10 +1,10 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        nums.sort()
-        return nums
+        for i in range(len(nums)-1,0,-1):
+            for j in range(i):
+                if nums[j]>nums[j+1]:
+                    nums[j],nums[j+1]=nums[j+1],nums[j]
+    
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
