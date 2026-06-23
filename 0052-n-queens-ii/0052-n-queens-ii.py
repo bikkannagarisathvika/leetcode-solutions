68ebe0badac88d930a2dce232b1c674f9ae5e5d8
Bikkannagari Sathvika Reddy <sathvikareddy2226@gmail.com>
2026-06-23 15:38:26 +0530

    Time: 11 ms (43.22%), Space: 19.4 MB (23.88%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        count=[0]
+        cols=set()
+        diag1=set()
+        diag2=set()
+        def bt(row):
+            if row==n:
+                count[0]+=1
+                return
+            for col in range(n):
+                if col in cols or (row-col) in diag1 or (row+col) in diag2:
+                    continue
+                cols.add(col)
+                diag1.add(row-col)
+                diag2.add(row+col)
+                bt(row+1)
+                cols.remove(col)
+                diag1.remove(row-col)
+                diag2.remove(row+col)
+        bt(0)
+        return count[0]
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
