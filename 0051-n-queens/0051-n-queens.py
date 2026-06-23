c981371a7ff1c12afb70428a80739e9453c59535
Bikkannagari Sathvika Reddy <sathvikareddy2226@gmail.com>
2026-06-23 14:25:34 +0530

    Time: 11 ms (66.85%), Space: 19.9 MB (9.9%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,34 +1,27 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        board=[["."] * n for _ in range(n)]
         res=[]
         cols=set()
-        diag1=set()  
-        diag2=set()  
-        def backtrack(row):
+        diag1=set()
+        diag2=set()
+        board=[["."]*n for _ in range(n)]
+        def bt(row):
             if row==n:
-                temp=[]
-                for r in board:
-                    temp.append("".join(r))
-                res.append(temp)
+                res.append(["".join(r) for r in board])
                 return
             for col in range(n):
-                if col in cols:
+                if (col in cols) or (row-col in diag1) or (row+col in diag2):
                     continue
-                if (row-col) in diag1:
-                    continue
-                if (row+col) in diag2:
-                    continue
-                board[row][col]="Q"
                 cols.add(col)
                 diag1.add(row-col)
                 diag2.add(row+col)
-                backtrack(row+1)
+                board[row][col]="Q"
+                bt(row+1)
                 board[row][col]="."
                 cols.remove(col)
                 diag1.remove(row-col)
                 diag2.remove(row+col)
-        backtrack(0)
+        bt(0)
         return res
 
 # Synced seamlessly with LeetHub Pro
